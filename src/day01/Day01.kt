4f57955d76f69755656a7223e143e72a8e4a388f
Jakub Havelka <jakub.havelka@vacuumlabs.com>
2021-12-01 14:17:24 +0100

    Day01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,15 +1,22 @@
+package day01
+
+import readInput
+
 fun main() {
+
+    fun <T : Comparable<T>> List<T>.risingCount(): Int = windowed(2).count { it.first() < it.last() }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.risingCount()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(3).map { it.sum() }.risingCount()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
